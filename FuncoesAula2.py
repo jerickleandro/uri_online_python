dfa1e313d859927ae0e9838d07f85e80ca7f8c21
Erick <j.erickleandro@gmail.com>
2019-08-25 03:41:20 -0300

    Funcao proximaLetra implemented

--- FuncoesAula2.py
+++ FuncoesAula2.py
@@ -2,7 +2,10 @@ def divAporB(a,b):
     if(a%b==0):
         return True
 
-teste = int(ord("a"))
-teste2 = (chr(teste+1))
-print(teste)
-print(teste2)
+def proximaLetra(a):
+        teste = int(ord(a))
+        return chr(teste+1)
+
+a = input("Digite uma letra")
+b = proximaLetra(a)
+print("A proxima letra é ",b)
